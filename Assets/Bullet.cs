73dceb8e54a05986dc254302ee637fb67ac845b3
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-02-02 22:26:09 -0500

    remove unnecessary box collider

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,11 +5,10 @@ public class Bullet : MonoBehaviour
     public float bulletSpeed = 6f;
     public float fireDelay = 0.5f;
     public float lifetime = 4.0f;
-    private BoxCollider2D boxCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+
     }
 
     // Update is called once per frame
