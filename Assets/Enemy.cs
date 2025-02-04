9cffbfd8b2c0c488d3a2100a11f19e3c258cc158
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-02-04 12:16:07 -0500

    fix bug with enemy points

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     public GameObject speedPowerup;
     public GameObject fireratePowerup;
     public int scoreValue;
-    public GameObject gameController;
+    private GameObject gameController;
     Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
